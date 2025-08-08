54503bdabe6c89c19f178567666325a3eb7a667e
Shreya Ganesh Shinde <shreyashinde2256@gmail.com>
2025-08-08 22:55:22 +0530

    Add Arrays

---
+++ Arrays/script.js
@@ -0,0 +1,43 @@
+//Arrays
+// let marks = [60,70,80,90];
+// console.log(marks);
+// console.log(marks.length);//property
+//looping over array
+ let cities = ["Pune","Mumbai","Delhi","Bengaluru"];
+ console.log(cities);
+
+// for(let city of cities){
+//     console.log(city);
+// }
+//Array Methods
+// 1) push()
+// 2) pop()
+// 3) toString()
+// let addedCity = cities.push("Kolkata");
+// console.log(cities);
+// let deletedCity = cities.pop();
+// console.log(cities);
+// 4) concat()
+// 5) unshift() -> to delete from starting
+// 6) shift() -> returns deleted value
+// 7) slice( startIndex, endIndex)
+// 8) splice(startIndex, delCount, newIndex)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
