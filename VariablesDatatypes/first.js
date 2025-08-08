fa0d8dc8b361dcb85ffe074d2722e3f23b8c942f
Shreya Ganesh Shinde <shreyashinde2256@gmail.com>
2025-08-08 23:21:55 +0530

    Add Variables and Datatypes

---
+++ VariablesDatatypes/first.js
@@ -0,0 +1,35 @@
+//on browser if we want to clear all then type => ctrl + L 
+/*
+console.log("Hi Shreya");
+console.log("This is my first javascript programme");
+
+ullName = "Shreya Ganesh Shinde"
+age = 24
+birthPlace = "Baramati"
+x = null
+y = undefined
+console.log(x);
+isFollow = false;
+console.log(isFollow);
+*/
+
+//Javascript is Case-Sensitive
+/*
+fullName ="Shreya"
+FullName ="SHREYA"
+console.log(FullName);
+*/
+
+//datatypes in js
+let name = "Shreya";
+console.log(name);
+
+var age = 24
+var age = 25
+var  age = 26
+console.log(age);
+
+//typeof - to check data type
+
+let yash = 24
+console.log(typeof(yash));
---
+++ VariablesDatatypes/second.js
@@ -0,0 +1,9 @@
+const student = {
+    fullName: "Shreya",
+    age : 22,
+    cgpa : 9.40,
+    isPass : true
+}
+
+console.log(student["fullName"]);
+console.log(student["age"]);
