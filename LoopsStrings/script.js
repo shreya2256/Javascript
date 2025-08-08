6ad51993bdd3380c259d5eed3df81ce3af2913d4
Shreya Ganesh Shinde <shreyashinde2256@gmail.com>
2025-08-08 23:06:23 +0530

    Add Loops and Strings

---
+++ LoopsStrings/script.js
@@ -0,0 +1,72 @@
+//for loop
+/* print 1 to 5
+for(let i = 1; i<=5; i++){
+    console.log("Hi Shreya");
+}
+console.log("loop has ended")
+
+//print sum 1 to 5
+let sum =0;
+for(let i = 1; i<=5; i++){
+    sum += i;//sum =15   
+}
+console.log("Sum 1 to 5 is", sum);
+console.log("Done");
+
+//calculate sum 1 to n
+let n=100;
+let sum =0;
+for(let i=0; i<=n;i++){
+    sum +=i;
+}
+console.log("Sum = ", sum);
+*/
+//while loop
+// while(condition){
+//     //do some work
+// }
+
+// let i = 1;
+// while(i<=5){
+//     console.log("Hello");
+//     i++;
+// }
+
+// do-while loop
+
+// do{
+//     //do some work
+// }while(condition);
+
+// let i = 20;
+// do{
+//     console.log("Shreya");
+//     i++;
+// }while(i<=10);
+
+// for-of Loop=> to help us to loop on strings and array , to do iteration across loop
+// Syntax
+// for(let val of strVar){
+//     //do some work
+// }
+
+// let str = "ApnaCollege";
+// for(let i of str){//iterator ->characters
+//     console.log("i = ", i);
+// }
+
+// for-in loop
+// Syntax
+// for(let key in objVar){
+//     //do some work
+// }
+// let student={
+//     name:"Shreya",
+//     age : 20,
+//     cgpa : 9.40,
+//     isPass : TextTrackCue,
+// }
+
+// for(let key in student){
+//     console.log("key =", key, ", value =", student[key]);
+// }
---
+++ LoopsStrings/scriptt.js
@@ -0,0 +1,44 @@
+// Strings in js
+// let str ="shreya";
+// console.log(str[4]);
+
+//Template Literals ``
+//`string text ${expression} string text`
+// let specialString = `This is Template Literal`;
+// console.log(specialString);
+
+// let obj ={
+//     item:"pen",
+//     price : 10,
+// };
+
+//console.log("The cost of",obj.item, "is", obj.price,"rupees");
+//after Template Literal
+//console.log(`The cost of ${obj.item} is ${obj.price} rupees`);
+//String Methods
+// 1) str.toUpperCase()
+// 2) str.toLowerCase()
+// 3) str.trim()//removes whitespaces
+// 4) str.slice(start, end?)//returns part of string
+// 5) str1.concat(str2)//join str2 with str1
+// 6) str.replace(searchVal, newVal)
+// 7) str.charAt(idx)
+let str = " apnacollege ";
+let str1 = "Shreya";
+let str2 ="Shinde";
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.trim());
+console.log(str.slice(0, 5));
+console.log(str1.concat(str2));
+console.log(str.replace("c","k"));
+console.log(str.charAt(1));
+
+
+
+
+
+
+
+
+
