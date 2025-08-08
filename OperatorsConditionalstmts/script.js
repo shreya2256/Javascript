8394cb1388a72c5cd127c2cb89f39be833bbe3b5
Shreya Ganesh Shinde <shreyashinde2256@gmail.com>
2025-08-08 23:16:14 +0530

    Add Operators and Conditional statementss

---
+++ OperatorsConditionalstmts/script.js
@@ -0,0 +1,78 @@
+//Arithmetic Operators
+/*
+let a = 5;
+let b = 2;
+
+console.log("a = ", a , "& b = " ,b);
+console.log("a+b = ", a+b);
+console.log("a-b = ", a-b);
+console.log("a*b = ", a*b);
+console.log("a/b = ", a/b);
+console.log("a % b = ", a % b);
+console.log("a ** b = ", a ** b);
+*/
+
+//Unary Operator
+/*
+let a = 5;
+let b = 2;
+
+console.log("a= ", a , "& b =", b);
+//pre-increment
+console.log("++a = ", ++a);//6
+console.log(a);//6
+//post-increment
+console.log("a++ = ", a++);//6
+console.log(a);//7
+
+console.log("a= ", a , "& b =", b);
+//pre-decrement
+console.log("--a = ", --a);
+console.log(a);
+//post-decrement
+console.log("a-- = ", a--);
+console.log(a);
+*/
+//Assignment Operators
+/*
+let a = 5;
+let b = 2;
+
+console.log("a=",a, "b=", b);
+
+a += 2
+console.log("a =", a);
+a -= 2
+console.log("a =", a);
+a *= 2
+console.log("a =",a);
+a %= 2
+console.log("a=",a);
+a **= 4
+console.log("a= ", a);
+*/
+//Comparison Operators
+/*
+let a = 5
+let b = 2
+console.log("a = ",a, " & b = ",b);
+console.log("5 == 2", a == b);
+console.log("5 != 2", a != b);
+console.log("5 >= 2", a>=b);
+console.log("5 <= 2", a<=b);
+console.log("5 === 2", a === b);
+*/
+//Logical Operators
+
+let  a = 6;
+let b = 5;
+console.log("a =", a, "& b =", b);
+//AND Operator
+let cond1 = a > b; //true
+let cond2 = a === 6 ; //true
+console.log("cond1 && cond2 = ", cond1 && cond2);
+//OR Operator
+console.log("cond1 || cond2 = ", cond1 || cond2);
+//NOT Operator
+console.log("!(6<5) = ",!(a<b));
+
---
+++ OperatorsConditionalstmts/scriptt.js
@@ -0,0 +1,52 @@
+//conditional statements
+/* if statement
+let age = 18;
+
+if(age > 18){
+    console.log("You can vote")
+}
+
+let mode = dark;
+let color;
+
+if (mode === "dark"){
+    color = "black";
+}
+
+if(mode === "light"){
+    color = "white";
+}
+
+console.log(color);
+*/
+
+//if-else 
+/*
+let num = 20;
+
+if(num % 2 === 0){
+    console.log(num, "is even");
+}else{
+    console.log(num, "is odd");
+}*/
+
+//if-else if- else
+/*
+let mode = "dark";
+let color;
+
+if(mode === "dark"){
+    color = "dark";
+}else if(mode === "pink"){
+    color = "pink";
+}else{
+    color = "white";
+}
+
+console.log(color);
+*/
+//Ternary Operator
+
+let age = 18;
+
+age >=18 ? console.log("adult"):console.log("not adult");
